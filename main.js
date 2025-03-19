ca34b09c9e069469616fdd4624f53771da1e2c89
shirocodes <ishirapt@gmail.com>
2025-03-20 02:44:14 +0300

    Completed the lab

--- main.js
+++ main.js
@@ -3,9 +3,53 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const likeHearts = document.querySelectorAll(".like-glyph");
 
+likeHearts.forEach(likeHeart => { //looping through the hearts and adding listener
+  likeHeart.addEventListener("click", () => {
+    console.log(likeHeart)
+    handleLike(likeHeart)
+  })
+})
 
+function handleLike(likeHeart) { //handle the heart click and send request to mimicserv..
+  console.log("sending request to mimicServerCall ....")
 
+  mimicServerCall()
+    .then(() => {
+      console.log("I have responded to you")
+      togglelikeHeart(likeHeart);
+    })
+    .catch((error) => {
+      console.log("I have received your error:", error)
+      showError(error);
+    })
+}
+
+function togglelikeHeart(likeHeart) {
+  if (likeHeart.textContent === EMPTY_HEART) {
+    console.log("I will change to red")
+    likeHeart.textContent = FULL_HEART;
+    likeHeart.classList.add("activated-heart") //make the heart red
+  } else {
+    likeHeart.textContent = EMPTY_HEART;
+    console.log("Ok i will not be red")
+    likeHeart.classList.remove("activated-heart") //no more red heart
+  }
+}
+
+function showError(errorMsg) {
+  const modal = document.getElementById("modal");
+  const modalMsg = document.getElementById("modal-message");
+
+  modalMsg.textContent = errorMsg;
+  modal.classList.add("hidden");
+}
+
+setTimeout(() => {
+  console.log("hiding error modal after 3 seconds...")
+  modal.classList.add("hidden")
+}, 3000)
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
